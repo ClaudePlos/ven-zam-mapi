043b0e2068d4a878497c3a9c96fffcecc60d43aa
k.skowronski <claude-plos@o2.pl>
2022-04-26 21:22:19 +0200

    added update bind position

--- frontend/views/feed/feed-view.ts
+++ frontend/views/feed/feed-view.ts
@@ -1,16 +1,28 @@
-import '@vaadin/grid/vaadin-grid';
+import '@vaadin/button';
 import '@vaadin/combo-box'
+import '@vaadin/grid/vaadin-grid';
+import '@vaadin/grid/vaadin-grid-column-group.js';
 import './components/claude-date';
+import '@vaadin/number-field';
+import '@vaadin/integer-field';
 
-import { html } from 'lit';
+import { html, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
+import {Notification} from "@vaadin/notification";
 import { View } from '../../views/view';
+import { Binder, field } from '@hilla/form';
+import { GridItemModel } from '@vaadin/grid';
 import { feedViewStore } from './feed-view-store';
 import KierunekKosztowVO from "Frontend/generated/pl/kskowronski/data/entity/mapi/KierunekKosztowVO";
 // @ts-ignore
 import {GrupaZywionychEndpoint, KierunekKosztowEndpoint, StanyZywionychEndpoint} from "Frontend/generated/endpoints";
 import GrupaZywionychVO from "Frontend/generated/pl/kskowronski/data/entity/mapi/GrupaZywionychVO";
 import StanZywionychNaDzienDTO from "Frontend/generated/pl/kskowronski/data/entity/mapi/StanZywionychNaDzienDTO";
+
+import "./feed-view.css"
+import StanZywionychNaDzienDTOModel
+    from "Frontend/generated/pl/kskowronski/data/entity/mapi/StanZywionychNaDzienDTOModel";
+//import '@vaadin/vaadin-lumo-styles/utility.js';
 
 @customElement('feed-view')
 export class FeedView extends View {
@@ -27,6 +39,7 @@ export class FeedView extends View {
 
     @state()
     private stanyZywionychNaDzien: StanZywionychNaDzienDTO[] = [];
+    private binder = new Binder(this, StanZywionychNaDzienDTOModel);
 
     async firstUpdated() {
         const kkList = await KierunekKosztowEndpoint.getAllKK();
@@ -34,7 +47,10 @@ export class FeedView extends View {
     }
 
     render() {
-        return html`<div>
+
+        const { model } = this.binder;
+
+        return html`<div style="width: 99%; height: 100%; padding-left: 5px">
             <div><claude-date></claude-date>
                 <vaadin-combo-box  label="Kierunek kosztów"
                                    .items="${this.kkList}"
@@ -54,14 +70,54 @@ export class FeedView extends View {
                                    label="Browser"
                                    helper-text="Wybierz grupę żywionych"
                 ></vaadin-combo-box>
+                <vaadin-button theme="secondary" @click=${this.save}>Zapisz</vaadin-button>
             </div>
-            <vaadin-grid .items="${this.stanyZywionychNaDzien}">
-                <vaadin-grid-column path="lp"></vaadin-grid-column>
-                <vaadin-grid-column path="dietaNazwa"></vaadin-grid-column>
-                <vaadin-grid-column path="sniadaniePlanIl"></vaadin-grid-column>
+            <vaadin-grid .items="${this.stanyZywionychNaDzien}" style="width: 99%; height: 88%" theme="column-borders">
+
+                <vaadin-grid-column path="lp" width="50px"></vaadin-grid-column>
+                <vaadin-grid-column path="dietaNazwa" width="300px"></vaadin-grid-column>
+  
+                
+                <vaadin-grid-column-group header="Planowanie">
+                <vaadin-grid-column header="Ś" path="sniadaniePlanIl" .renderer="${this.valueRenderer}"></vaadin-grid-column>
                 <vaadin-grid-column path="drugieSniadaniePlanIl"></vaadin-grid-column>
+                <vaadin-grid-column path="obiadPlanIl"></vaadin-grid-column>
+                <vaadin-grid-column path="podwieczorekPlanIl"></vaadin-grid-column>
+                <vaadin-grid-column path="kolacjaPlanIl"></vaadin-grid-column>
+                <vaadin-grid-column path="posilekNocnyPlanIl"></vaadin-grid-column>
+                <vaadin-grid-column path="szUwagi"></vaadin-grid-column>
+                </vaadin-grid-column-group>
+                
+
+                <vaadin-grid-column-group header="Korekta">
+                <vaadin-grid-column path="sniadanieKorIl"></vaadin-grid-column>
+                <vaadin-grid-column path="drugieSniadanieKorIl"></vaadin-grid-column>
+                <vaadin-grid-column path="obiadKorIl"></vaadin-grid-column>
+                <vaadin-grid-column path="podwieczorekKorIl"></vaadin-grid-column>
+                <vaadin-grid-column path="kolacjaKorIl"></vaadin-grid-column>
+                <vaadin-grid-column path="posilekNocnyKorIl"></vaadin-grid-column>
+                </vaadin-grid-column-group>
             </vaadin-grid>
     </div>`;
+    }
+
+    private valueRenderer = (root: HTMLElement, _: HTMLElement, model: GridItemModel<StanZywionychNaDzienDTO>) => {
+        render(html` <vaadin-integer-field has-controls @value-changed=${(e: CustomEvent) => this.updateTodoState(model.item, e.detail.value)}
+                                           value="${model.item.sniadaniePlanIl}"></vaadin-integer-field>`, root);
+    };
+
+    // private valueRenderer = (root: HTMLElement, _: HTMLElement, model: GridItemModel<StanZywionychNaDzienDTO>) => {
+    //     render(html` <input type="number" id="fname" name="fname" value="${model.item.sniadaniePlanIl}"
+    //                         onchange="${model.item.sniadaniePlanIl} == this.value"
+    //             style="border: none;width: 50px;font-size: 16px"></input>`, root);
+    // };
+
+    updateTodoState( item: StanZywionychNaDzienDTO, value: number){
+        item.sniadaniePlanIl = value;
+    }
+
+    inputChange(e: CustomEvent) {
+        Notification.show("zmienione");
     }
 
     kkChanged(e: CustomEvent) {
@@ -82,6 +138,10 @@ export class FeedView extends View {
     async getStanyZywionychNaDzien() {
         const stanyZywionychNaDzien = await StanyZywionychEndpoint.pobierzStanZywionychWdniuDlaGZ(feedViewStore.startDate, this.idGZ, this.sortType);
         this.stanyZywionychNaDzien = stanyZywionychNaDzien;
+    }
+
+    async save() {
+        Notification.show("Zapisane");
     }
 
     connectedCallback() {
